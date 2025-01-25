326af47ba02845ef32a01529df560de252ad05a7
Rafal Adamczyk <rafal.adamczyk44@gmail.com>
2025-01-25 17:21:54 +0100

    Adding exception block for web search

--- assistants/holiday_planner.py
+++ assistants/holiday_planner.py
@@ -1,3 +1,4 @@
+from duckduckgo_search.exceptions import DuckDuckGoSearchException
 from langchain_huggingface import HuggingFaceEndpoint, HuggingFaceEmbeddings
 from langchain_community.tools import DuckDuckGoSearchRun
 
@@ -21,17 +22,33 @@ class HolidayPlanAssistant:
         self.flight_search_prompt = get_flight_search_prompt()
         self.accommodation_prompt = get_accomodation_search_prompt()
 
-    def search_queries(self, search_q):
-        search_results = []
+    def search_queries(self, search_q) -> str:
+        """
+        Method for organizing browser searching
+        :param search_q: Query to be searched
+        :return: String
+        """
 
-        for query in search_q:
-            results = self.search.run(query)
-            search_results.append(results)
+        search_results = []
+        try:
+            for query in search_q:
+                results = self.search.run(query)
+                search_results.append(results)
+        except DuckDuckGoSearchException:
+            print("Can't search at the moment :(")
 
         return "\n".join(search_results)
 
     def search_flights(self, origin:str, destination:str, date:str, n_days:int) -> str:
-
+        """
+        Ask agent to return list of the best fitting flights in given dates based on a scoring system
+        (Price, how close it is to the selected dates, baggage policy).
+        :param origin: City to depart from
+        :param destination: City of final destination
+        :param date: Flight date
+        :param n_days: Days before return
+        :return: String
+        """
         search_queries = [
             f"flights from {origin} to {destination} on {date} site:kiwi.com",
             f"flights from {origin} to {destination} on {date} site:skyscanner.com"
@@ -72,5 +89,5 @@ class HolidayPlanAssistant:
 
         return agent_response
 
-test = HolidayPlanAssistant()
-# print(test.search_flights('Warsaw', 'Madrid',  '25.02.2025', 6))
+    def concat_search_results(self, search_results_flight, search_results_accommodations):
+        pass
